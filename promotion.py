e3d6b67d24342696b7a67aef602ffce68d5bdf11
The Oracle Software Owner User <oracle@vlt-vm.sub06050050430.vltvcn.oraclevcn.com>
2020-06-12 01:14:56 +0000

    Add database connection

--- promotion.py
+++ promotion.py
@@ -3,6 +3,7 @@ Simple Python application to show CI/CD capabilities.
 """
 
 from bottle import Bottle, run
+import cx_Oracle
 
 app = Bottle()
 
@@ -22,5 +23,16 @@ def decrease(salary, amount):
     return str(int(salary) - int(amount))
 
 
+@app.route('/conn')
+def conn():
+    return str(connection.version)
+
+
 if __name__ == '__main__':
+    DBUSER = 'hr'
+    DBPASS = 'WelCom3#2020_'
+    DBSERV = 'VLTDB_TP'
+    conn_string = DBUSER + '/' + DBPASS + '@' + DBSERV
+    connection = cx_Oracle.connect(conn_string)
     run(app, host='0.0.0.0', port=8080)
+    connection.close()
